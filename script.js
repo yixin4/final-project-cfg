7e182be6abf59d9e4de69475fc14ca9838689835
yixin4 <yinyixin404@gmail.com>
2023-07-25 23:47:39 +0200

    wrong answer can not be chosen

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++
   setNextQuestion()
